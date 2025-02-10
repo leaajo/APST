802133100c13c1a4836f3d3f6a7513d3425b453c
leaajo <lea.joseph@icloud.com>
2025-02-10 14:13:43 +0100

    Ajout du fichier compressé data

--- viz/tri.py
+++ viz/tri.py
@@ -1,4 +1,11 @@
 import numpy as np
 import pandas as pd
 import matplotlib as plt
+import gzip
+import csv
 
+# Décompression du fichier data :
+with gzip.open('data/data.csv.gz', 'rt', newline='') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        print(row)
